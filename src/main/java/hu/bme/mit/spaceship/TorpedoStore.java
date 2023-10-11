c7cde7fe4fdd2a9ae9103c6590ce69742e1bbf0a
Lakatos Ádám <dev@m1mir.org>
2023-10-11 09:55:12 +0200

    Komment.

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -32,6 +32,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
+      //Kivétel dobás
       throw new IllegalArgumentException("numberOfTorpedos");
     }
 
